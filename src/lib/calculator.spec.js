5e9c8e49e1a6ecf979c7199d8175d66983ba33f2
Juliano Dotto <jdotto95@gmail.com>
2022-02-23 11:04:57 -0300

    prettier format on calculator classes

--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -1,23 +1,23 @@
-const {sum} = require('./calculator');
+const { sum } = require('./calculator');
 
 it('should sum 2 and 2 and the result must be 4', () => {
-  expect(sum(2,2)).toBe(4);
+  expect(sum(2, 2)).toBe(4);
 });
 
 it('should sum 2 and 2 even if one of them is a string and the result must be 4', () => {
-  expect(sum(2,'2')).toBe(4);
+  expect(sum(2, '2')).toBe(4);
 });
 
 it('should throw an error if what is provided to the method was not a number', () => {
   expect(() => {
-    sum('', '2')
+    sum('', '2');
   }).toThrowError();
 
   expect(() => {
-    sum([], {})
+    sum([], {});
   }).toThrowError();
 
   expect(() => {
-    sum()
+    sum();
   }).toThrowError();
 });
